f41a6e05cc5c0fad01e2c863e27467b9d5de5298
Ed_Andrew <ederandrew0028@gmail.com>
2018-06-22 22:58:36 -0300

    Finalizando a programação do classificados. Corrigindo a paginação e filtros dos anuncios

--- index.php
+++ index.php
@@ -7,7 +7,17 @@
     $u = new Usuarios();//o require ja esta no header
     $c = new Categorias();
 
-    $total_anuncios = $a->getTotalAnuncios();
+    $filtros = array(
+        'categoria' => '',
+        'preco' => '',
+        'estado' => ''
+    );
+
+    if(isset($_GET['filtros'])){
+        $filtros = $_GET['filtros'];
+    }
+
+    $total_anuncios = $a->getTotalAnuncios($filtros);
     $total_usuarios = $u->getTotalUsuarios();
 
     //Pegar página atual
@@ -18,7 +28,7 @@
     $por_pagina = 2;
     $total_paginas = ceil($total_anuncios / $por_pagina);
 
-    $anuncios = $a->getUltimosAnuncios($p, 2);//pegar página como parâmetro
+    $anuncios = $a->getUltimosAnuncios($p, $por_pagina, $filtros);//pegar página como parâmetro
     $categorias = $c->getLista();
 ?>
 <section>
@@ -40,29 +50,29 @@
                         <select id="categoria" name="filtros[categoria]" class="form-control">
                             <option></option>
                             <?php foreach($categorias as $cat): ?>
-                            <option value="<?php echo $cat['id']; ?>"><?php echo utf8_encode($cat['nome'])?></option>
+                            <option value="<?php echo $cat['id']; ?>" <?php echo ($cat['id']==$filtros['categoria'])?'selected="selected"':'';?>><?php echo utf8_encode($cat['nome'])?></option>
                             <?php endforeach; ?>
                         </select>
                     </div>
 
                     <div class="form-group">
                         <label for="preco">Preço:</label>
-                        <select id="preco" name="filtros[categoria]" class="form-control">
+                        <select id="preco" name="filtros[preco]" class="form-control">
                             <option></option>
-                           <option value="0-50">R$ 0 - 50</option>
-                           <option value="51-100">R$ 51 - 100</option>
-                           <option value="101-200">R$ 101 - 200</option>
-                           <option value="201-500">R$ 201 - 500</option>
+                           <option value="0-50" <?php echo ($filtros['preco']=='0-50')?'selected="selected"':'';?>>R$ 0 - 50</option>
+                           <option value="51-100" <?php echo ($filtros['preco']=='51-100')?'selected="selected"':'';?>>R$ 51 - 100</option>
+                           <option value="101-200" <?php echo ($filtros['preco']=='101-200')?'selected="selected"':'';?>>R$ 101 - 200</option>
+                           <option value="201-500" <?php echo ($filtros['preco']=='201-500')?'selected="selected"':'';?>>R$ 201 - 500</option>
                         </select>
                     </div>
 
                     <div class="form-group">
                         <label for="estado">Estado de Conservação:</label>
-                        <select id="estado" name="filtros[categoria]" class="form-control">
+                        <select id="estado" name="filtros[estado]" class="form-control">
                             <option></option>
-                           <option value="0">Ruim</option>
-                           <option value="1">Bom</option>
-                           <option value="2">Ótimo</option>
+                           <option value="0" <?php echo ($filtros['estado']=='0')?'selected="selected"':'';?>>Ruim</option>
+                           <option value="1" <?php echo ($filtros['estado']=='1')?'selected="selected"':'';?>>Bom</option>
+                           <option value="2" <?php echo ($filtros['estado']=='2')?'selected="selected"':'';?>>Ótimo</option>
                         </select>
                     </div>
                     <div class="form-group">
@@ -94,7 +104,11 @@
                 </table>
                 <ul class="pagination">
                     <?php for($q=1;$q<=$total_paginas;$q++): ?>
-                    <li class="page-item"><a class="page-link" href="index.php?p=<?php echo $q; ?>"><?php echo $q?></a></li>
+                    <li class="page-item"><a class="page-link" href="index.php?<?php
+                     $w = $_GET;
+                     $w['p'] = $q; 
+                     echo http_build_query($w);
+                    ?>"><?php echo $q?></a></li>
                     <?php endfor; ?>
                 </ul>
             </div>
